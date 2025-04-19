58adb053145735528c6385f9907a4dd3b8ba1272
AllanNara <allannara@outlook.com>
2025-04-18 23:23:39 -0300

    segunda parte, chat finalizado

--- 6-aplicacion-chat-con-websockets/src/app.js
+++ 6-aplicacion-chat-con-websockets/src/app.js
@@ -9,6 +9,23 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
+const messages = [];
+io.on("connection", (socket) => {
+  console.log("Un nuevo cliente se ha conectado")
+  console.log("ID del cliente: ", socket.id)
+
+  socket.emit("all-msgs", messages)
+
+  socket.on("new-msg", (data) => {
+    messages.unshift(data)
+    io.emit("all-msgs", messages)
+  })
+
+  socket.on("login", (data) => {
+    socket.broadcast.emit("new-user", data)
+  })
+})
+
 app.engine("handlebars", hbs.engine());
 app.set("views", import.meta.dirname + "/views");
 app.set("view engine", "handlebars");
